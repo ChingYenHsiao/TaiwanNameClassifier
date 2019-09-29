f588ae7172be100517bdb4e18b700c5792fc12c3
ChingYenHsiao <n79122@gmail.com>
2019-09-29 19:33:27 +0800

    [Add] library files

---
+++ classifier/mylib/PrintException.py
@@ -0,0 +1,9 @@
+#https://stackoverflow.com/questions/14519177/python-exception-handling-line-number
+def PrintException():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    print ('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
---
+++ classifier/mylib/function_list.py
@@ -0,0 +1,24 @@
+from mylib.PrintException  import PrintException
+from mylib.is_chinese import is_chinese
+from mylib.is_number import is_number
+
+
+from mylib.Taiwan_name_seperate import GetFirstName
+from mylib.Taiwan_name_seperate import GetLastName
+from mylib.Taiwan_name_seperate import checkLastName
+from mylib.Taiwan_name_seperate import is_biFirstName
+
+#find 名字是否含非繁體字會用之字的簡體字
+from mylib.findSimName import findSimName
+
+
+from mylib.fortune_calculator import Get_stroke
+from mylib.fortune_calculator import get_stroke_state
+from mylib.fortune_calculator import get_talent_state
+from mylib.fortune_calculator import get_talent_type
+from mylib.fortune_calculator import test_name_Fortune_telling
+from mylib.fortune_calculator import stroke_earth
+from mylib.fortune_calculator import stroke_heaven
+from mylib.fortune_calculator import stroke_man
+from mylib.fortune_calculator import stroke_outside
+from mylib.fortune_calculator import stroke_total
